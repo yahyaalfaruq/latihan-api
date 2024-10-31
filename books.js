c6c123443f0a15a8d33a353ca56cafc6724ffb7d
jhon <jhonelfarq@gmail.com>
2024-10-31 12:34:54 +0700

    first commit

--- books.js
+++ books.js
@@ -1,6 +1,7 @@
-const http = require("http");
-const url = require("url");
+const MyExpress = require("express");
+const app = new MyExpress();
 
+app.use(MyExpress().json());
 let books = [
   {
     book_id: 1,
@@ -13,108 +14,64 @@ let books = [
   },
 ];
 
-let nextBookId = 2;
+app.post("/api/books", (req, res) => {
+  const newBook = {
+    book_id: books.length + 1,
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    year: req.body.year,
+    price: req.body.price,
+    stock: req.body.stock,
+  };
+  books.push(newBook);
+  res.setHeader(200, { "Content-type": "application/json" });
+  res.end(JSON.stringify(newBook));
+});
 
-const respondWithJSON = (res, statusCode, data) => {
-  res.writeHead(statusCode, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
-};
+app.get("/api/books", (req, res) => {
+  res.setHeader(200, { "Content-type": "application/json" });
+  res.end(JSON.stringify(books));
+});
 
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  let { pathname } = parsedUrl;
-  const method = req.method;
-
-  if (pathname === "/api/books" && method === "POST") {
-    createBook(req, res);
-  } else if (pathname === "/api/books" && method === "GET") {
-    readBooks(res);
-  } else if (pathname.startsWith("/api/books/") && method === "GET") {
-    const id = pathname.split("/")[3];
-    readBookById(res, id);
-  } else if (pathname.startsWith("/api/books/") && method === "PUT") {
-    const id = pathname.split("/")[3];
-    updateBook(req, res, id);
-  } else if (pathname.startsWith("/api/books/") && method === "DELETE") {
-    const id = pathname.split("/")[3];
-    deleteBook(res, id);
+app.get("/api/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const book = books.find((p) => p.id === bookId);
+  if (book) {
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify(book));
   } else {
     res.statusCode = 404;
-    res.end("Not Found");
+    res.end("book not found");
   }
 });
 
-const createBook = (req, res) => {
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-
-  req.on("end", () => {
-    const { title, author, genre, year, price, stock } = JSON.parse(body);
-    const newBook = {
-      book_id: nextBookId++,
-      title,
-      author,
-      genre,
-      year,
-      price,
-      stock,
-    };
-    books.push(newBook);
-    respondWithJSON(res, 201, {
-      message: "Buku berhasil ditambahkan",
-      book_id: newBook.book_id,
-    });
-  });
-};
-
-const readBooks = (res) => {
-  respondWithJSON(res, 200, books);
-};
-
-const readBookById = (res, id) => {
-  const book = books.find((b) => b.book_id === parseInt(id));
-  if (book) {
-    respondWithJSON(res, 200, book);
-  } else {
-    respondWithJSON(res, 404, { message: "Buku tidak ditemukan" });
-  }
-};
-
-const updateBook = (req, res, id) => {
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-
-  req.on("end", () => {
-    const { title, author, genre, year, price, stock } = JSON.parse(body);
-    const book = books.find((b) => b.book_id === parseInt(id));
-    if (book) {
-      book.title = title;
-      book.author = author;
-      book.genre = genre;
-      book.year = year;
-      book.price = price;
-      book.stock = stock;
-      respondWithJSON(res, 200, { message: "Buku berhasil diperbarui" });
-    } else {
-      respondWithJSON(res, 404, { message: "Buku tidak ditemukan" });
-    }
-  });
-};
-
-const deleteBook = (res, id) => {
-  const bookIndex = books.findIndex((b) => b.book_id === parseInt(id));
+app.put("/api/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const bookIndex = books.findIndex((p) => p.id === bookId);
   if (bookIndex !== -1) {
-    books.splice(bookIndex, 1);
-    respondWithJSON(res, 200, { message: "Buku berhasil dihapus" });
+    books[bookIndex].name = req.body.name;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(books[bookIndex]));
   } else {
-    respondWithJSON(res, 404, { message: "Buku tidak ditemukan" });
+    res.statusCode = 404;
+    res.end("Book not found");
   }
-};
+});
 
-server.listen(3000, () => {
+app.delete('/api/books/:id', (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const bookIndex = books.findIndex((p) => p.id === bookId);
+  if (bookIndex !== -1) {
+    const deletedBook = books.splice(bookIndex, 1);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(bookIndex[0]));
+  } else {
+    res.statusCode = 404;
+    res.end('Book not found');
+  }
+});
+
+app.listen(3000, () => {
   console.log("Books API listening on http://localhost:3000");
 });
